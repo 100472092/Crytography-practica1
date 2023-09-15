4dd55c730fdecf71e3accbfa7b4cc9b7aa62ee8b
CLopMan <cesarlopman@gmail.com>
2023-09-15 23:30:29 +0200

    Extract sql_code

--- src/main/data_base_gestor.py
+++ src/main/data_base_gestor.py
@@ -41,11 +41,7 @@ class DataBase:
             self.open()
             # restauración de la base de datos
             self.base.executescript(
-                """
-                DROP TABLE USER_CREDS;
-                DROP TABLE USER_SUBJ;
-                DROP TABLE USER_EVENT;
-                """
+                sql_scripts.DROP_TABLES
             )
             self.close()
 
@@ -55,31 +51,7 @@ class DataBase:
             # inicializar
             # TODO: mover el scrip de sql a otro archivo e importarlo
             self.base.executescript(
-                """
-                CREATE TABLE USER_CREDS (
-                    USER_NAME TEXT PRIMARY KEY, 
-                    PASSWORD TEXT NOT NULL
-                );
-                
-                CREATE TABLE USER_SUBJ (
-                    USER_NAME TEXT,
-                    SUBJECT TEXT,
-                    
-                    PRIMARY KEY(USER_NAME, SUBJECT),
-                    FOREIGN KEY(USER_NAME) REFERENCES USER_CREDS(USER_NAME)
-                );
-            
-                CREATE TABLE USER_EVENT (
-                    USER_NAME TEXT,
-                    SUBJECT TEXT,
-                    FECHA DATE,
-                    TIPO TEXT,
-                    NOTA INTEGER,
-                    
-                    PRIMARY KEY(USER_NAME, SUBJECT, FECHA, TIPO),
-                    FOREIGN KEY(USER_NAME, SUBJECT) REFERENCES USER_SUBJ(USER_NAME, SUBJECT)
-                );
-                """
+                sql_scripts.CREATE_TABLES
             )
             self.close()
 
